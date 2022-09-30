38e191b76f69ea349f450e498a61e1b8562bdc4e
OlgaGord <gordeewa06@gmail.com>
2022-09-30 19:50:51 +0500

    Задача 41 решена

---
+++ HomeWork/Task41/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int M = 5;
+Console.WriteLine($"Введите {M} чисел");
+int[] arr = new int[M];
+
+int[] Massiv (int M)
+{
+    for (int i = 0; i<M; i++)
+    {
+    Console.WriteLine($"Введите {i+1} число: ");
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+
+int Pos(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < M; i++)
+    {
+        if (array[i] > 0) result += 1;
+    }
+    return result;
+}
+
+arr = Massiv(M);
+Console.WriteLine($"Количество положительных чисел: {Pos(arr)}");
